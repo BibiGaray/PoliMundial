27fc7f299e8e76d8d496ed8fdce054ca7af8c21c
BibiGaray <theabig805@gmail.com>
2026-05-12 23:37:06 -0300

    Update torneo.py

---
+++ models/torneo.py
@@ -0,0 +1,6 @@
+class torneo:
+    def __init__(self, nombre = "", inicio = "", fin = ""):
+        self.nombre = nombre
+        self.inicio = inicio
+        self.fin = fin
+        self.cerrar_confi = False
