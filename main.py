6d75d62baffd1d536fa28efb567f3ee17d430143
BibiGaray <theabig805@gmail.com>
2026-05-13 00:04:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from models.equipo import Equipo
+from services.torneo_service import registrar_torneo
+from services.torneo_service import guardar_torneo
 from services.reportes_service import guardar_equipo
 from services.reportes_service import mostrar_equipos
 from services.partido_service import registrar_equipo
+
 while True:
     print("\n=== POLIMUNDIAL ===")
-    print("1. Registrar equipo")
-    print("2. Registrar Resultados")
-    print("3. Ver Informes")
-    print("4. Salir")
+    print("1. Configuración del Torneo. ")
+    print("2. Registrar Resultados. ")
+    print("3. Ver Informes. ")
+    print("4. Salir. ")
     # ------------------------------------------------------------------------------------------------------------------------------------ #
     opcion = input("Seleccione una opción: ")
     match opcion:
 
         case "1":
-            print("\n=== REGISTRAR EQUIPO ===")
-            team = registrar_equipo("team")
-            guardar_equipo(team)
+            print("\n=== CONFIGURACIÓN DEL TORNEO ===")
+            torneo = registrar_torneo("torneo")
+            guardar_torneo(torneo)
+            print("\n=== REGISTRAR EQUIPO 1 ===")
+            team1 = registrar_equipo("team1")
+            guardar_equipo(team1)
+            print("\n=== REGISTRAR EQUIPO 2 ===")
+            team2 = registrar_equipo("team2")
+            guardar_equipo(team2)
             print("Torneo registrado correctamente.")
 
         case "2":
